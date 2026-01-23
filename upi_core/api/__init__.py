987ecb051539bb13b21b317d3574ad1d7b63d70f
Hector Mozo <hector.mozo@neureonmindfluxlab.org>
2026-01-23 17:57:17 -0500

    api: add api package initializer

---
+++ upi_core/api/__init__.py
@@ -0,0 +1,17 @@
+"""
+Public API types for UPI core.
+"""
+
+from .types import (
+    CaseSpec,
+    UseSelector,
+    StageSpec,
+    PipelineSpec,
+)
+
+__all__ = [
+    "CaseSpec",
+    "UseSelector",
+    "StageSpec",
+    "PipelineSpec",
+]
